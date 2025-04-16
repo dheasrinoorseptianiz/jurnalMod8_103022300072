ec652dbbbed74d9c27ca99b3884218799f611c1e
PRAKTIKAN <PRAKTIKAN@DESKTOP-MU6PA4D>
2025-04-16 17:05:14 +0700

    aplikasi bank transfer dengan runtime configuration

---
+++ BankTransferConfig.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace JurnalModul8_103022300072
+{
+    class Transfer
+    {
+        public int threshold { get; set; }
+        public int low_fee { get; set; }
+        public int high_fee { get; set; }
+    }
+
+    class Confirmation
+    {
+        public string en { get; set; }
+        public string id { get; set; }
+    }
+
+    class BankTransferConfig
+    {
+        public string lang { get; set; }
+        public Transfer transfer { get; set; }
+        public string[] methods { get; set; }
+        public Confirmation confirmation { get; set; }
+
+        public BankTransferConfig()
+        {
+
+        }
+
+        public BankTransferConfig(string lang, Transfer transfer, string[] methods, Confirmation confirmation)
+        {
+            this.lang = lang;
+            this.transfer = transfer;
+            this.methods = methods;
+            this.confirmation = confirmation;
+        }
+
+        public void WriteNewConfigFile(string url)
+        {
+            if (url != null)
+            {
+                try
+                {
+                    JsonSerializerOptions jsonOptions = new JsonSerializerOptions()
+                    {
+                        WriteIndented = true
+                    };
+
+                    string jsonStr = JsonSerializer.Serialize(this, jsonOptions);
+                    File.WriteAllText(url, jsonStr);
+                }
+                catch (Exception e)
+                {
+                    Console.Error.WriteLine("Terjadi kesalahan dalam write config file! " + e.Message);
+                }
+            }
+            else
+            {
+                Console.Error.WriteLine("Tidak ada file pada url!");
+            }
+        }
+
+        public void ReadConfigFile(string url)
+        {
+            if (url != null)
+            {
+                try
+                {
+                    var json = File.ReadAllText(url);
+                    var bankConfig = JsonSerializer.Deserialize<BankTransferConfig>(json);
+
+                    lang = bankConfig!.lang;
+                    transfer = bankConfig.transfer;
+                    methods = bankConfig.methods;
+                    confirmation = bankConfig.confirmation;
+                }
+                catch (Exception e)
+                {
+                    Console.Error.WriteLine("Terjadi kesalahan dalam read config file! " + e.Message);
+                }
+            }
+            else
+            {
+                Console.Error.WriteLine("Tidak ada file pada url!");
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,61 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using JurnalModul8_103022300072;
+
+ class Program
+{
+    private const String url = @"../../../bank_transfer_config.json";
+
+    public static void Main(string[] args)
+    {
+        BankTransferConfig bankConfig = new BankTransferConfig();
+        bankConfig.ReadConfigFile(url);
+
+        bool defaultLang = bankConfig.lang == "en";
+
+        Console.WriteLine(defaultLang ? "Welcome" : "Selamat Datang!");
+        Console.WriteLine(defaultLang ? "Program Language!: " : "Bahasa Saat Ini!: " + bankConfig.lang);
+
+        try
+        {
+            Console.Write(defaultLang ? "Please insert the amount of money to transfer: " : "Masukkan jumlah uang yang akan di-transfer: ");
+            int amount = Convert.ToInt32(Console.ReadLine());
+
+            int totalPayed = 0;
+
+            Console.WriteLine(defaultLang ? "Select payment method: " : "Pilih metode pembayaran: ");
+            string[] methodPayment = bankConfig.methods;
+
+            for (int i = 0; i < methodPayment.Length; i++)
+            {
+                Console.WriteLine(i + " " + methodPayment[i]);
+            }
+
+            Console.Write(defaultLang ? "Choose number: " : "Pilih nomor: ");
+            int? choosenMethodPayment = Convert.ToInt32(Console.ReadLine());
+
+            if (choosenMethodPayment != null)
+            {
+                if (amount <= bankConfig.transfer.threshold)
+                {
+                    int fee = bankConfig.transfer.low_fee;
+
+                    totalPayed = amount + fee;
+                    Console.WriteLine(defaultLang ? $"Transfer fee: {fee} and Total amount: {totalPayed}" : $"Biaya transfer: {fee} and Total biaya: {totalPayed}");
+                }
+                else if (amount >= bankConfig.transfer.threshold)
+                {
+                    int fee = bankConfig.transfer.high_fee;
+
+                    totalPayed = amount + fee;
+                    Console.WriteLine(defaultLang ? $"Transfer fee: {fee} and Total amount: {totalPayed}" : $"Biaya transfer: {fee} and Total biaya: {totalPayed}");
+                }
+
+                Console.WriteLine(defaultLang ? "Transfer is completed!" : "Transfer berhasil!");
+            }
+        }
+        catch
+        {
+            Console.WriteLine(defaultLang ? "Transfer is cancelled!" : "Transfer dibatalkan!");
+        }
+    }
+}
